5f619c6dba22a0c3d74799650e61681b95d98eee
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-04-26 19:23:05 +0100

    add about me section to the layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css";
 import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
+import AboutMe from "./components/AboutMe";
 
 function App() {
   return (
-    <div className="container mx-auto bg-primary h-screen">
+    <div className="container mx-auto bg-primary h-screen antialiased">
       <Navigation />
       <Hero />
+      <AboutMe />
     </div>
   );
 }
