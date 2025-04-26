c8080e596dd2258e874529cd6e2e1c529632d70c
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-04-26 12:59:57 +0100

    Add the mobile drop menu and transition effects

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,9 +4,7 @@ import hamburger from "../assets/vector.svg";
 import light from "../assets/light.svg";
 import leftSpotlight from "../assets/left-spotlight.svg";
 import rightSpotlight from "../assets/right-spotlight.svg";
-import pattern from "../assets/pattern.svg";
 import gridPattern from "../assets/grid-pattern.svg";
-import bgPattern from "../assets/bg-pattern.svg";
 import "../style.css";
 import { useState } from "react";
 
@@ -16,29 +14,30 @@ const Navigation = () => {
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
+
   return (
     <div className="container mx-auto p-4 font-montserrat relative z-10">
       <img
         src={leftSpotlight}
         alt="Left Spotlight"
-        className="absolute top-0 left-0"
+        className="absolute top-0 left-0 "
       />
       <img
         src={rightSpotlight}
-        alt="Left Spotlight"
+        alt="Right Spotlight"
         className="absolute top-0 right-0"
       />
       <img
         src={gridPattern}
         alt="Pattern"
-        className="absolute top-10 left-1/2 transform -translate-x-1/2 -z-10 mx-auto" 
+        className="absolute top-10 left-1/2 transform -translate-x-1/2 -z-10 max-w-full"
       />
       <nav className="flex justify-between items-center md:w-[85%] mx-auto py-2">
         <div className="flex items-center gap-1 z-20">
           <img
             src={logo}
             alt="Logo"
-            className="h-8 w-8 inline-block mr-2 cursor-pointer "
+            className="h-8 w-8 inline-block mr-2 cursor-pointer"
           />
           <span className="text-white hidden md:block text-lg font-bold cursor-pointer">
             Abdulbasset Benz.
@@ -66,15 +65,11 @@ const Navigation = () => {
                 Projects
               </a>
             </li>
-            <li>
-              <a className="hover:text-purple-400" href="#contact">
-                Contact
-              </a>
-            </li>
+            
           </ul>
         </div>
         <div className="hidden md:flex items-center space-x-10 z-10">
-          <button className="md:block border-1 border-purple-300 text-white font-semibold px-4 py-2 rounded-lg hover:bg-purple-700 hover:text-white transition duration-300 cursor-pointer">
+          <button className="md:block text-white font-semibold px-4 py-2 rounded-lg hover:bg-purple-700 border border-purple-700 hover:text-white transition duration-300 cursor-pointer">
             Contact me
           </button>
 
@@ -83,6 +78,48 @@ const Navigation = () => {
             alt="Light Mode"
             className="h-8 w-8 hidden md:inline-block cursor-pointer hover:rotate-90 transition duration-300 hover:fill-purple-500"
           />
+        </div>
+        <img
+          src={hamburger}
+          alt="Menu"
+          className="h-8 w-8 inline-block md:hidden cursor-pointer z-20"
+          onClick={toggleMenu}
+        />
+
+        <div
+          className={`absolute lg:hidden top-20 left-0 w-full flex flex-col items-center gap-6 font-semibold text-lg ${
+            isMenuOpen
+              ? "opacity-100 translate-y-0"
+              : "opacity-0 -translate-y-5"
+          } transition-all duration-300 ease-in-out 
+   bg-purple-300/10 backdrop-blur-lg rounded-lg shadow-lg`}
+        >
+          <ul className="flex flex-col items-center gap-6 text-slate-50 p-4">
+            <li>
+              <a className="hover:text-purple-400" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="hover:text-purple-400" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="hover:text-purple-400" href="#about">
+                Services
+              </a>
+            </li>
+            <li>
+              <a className="hover:text-purple-400" href="#about">
+                Projects
+              </a>
+            </li>
+
+            <button className=" text-black bg-purple-300 font-semibold px-4 py-2 rounded-lg  hover:text-purple-900 transition duration-300 cursor-pointer">
+              Contact me
+            </button>
+          </ul>
         </div>
       </nav>
     </div>
