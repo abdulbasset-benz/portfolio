912a14c6cd7afa4cb87b4cef0b2c018e9d7ac96c
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-04-26 14:57:02 +0100

    add hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import './style.css';
-import Navigation from './components/Navigation';
+import React from "react";
+import "./style.css";
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
 
 function App() {
-  
-
   return (
     <div className="container mx-auto bg-primary h-screen">
-     <Navigation />
-      
-     
+      <Navigation />
+      <Hero />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
