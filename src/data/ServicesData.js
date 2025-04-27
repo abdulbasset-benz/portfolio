bbb0b3fb965b1ee1e7146ff272f2981f5e330728
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-04-27 14:16:02 +0100

    data

---
+++ src/data/ServicesData.js
@@ -0,0 +1,37 @@
+import code from '../assets/code.svg';
+import ecommerce from '../assets/ecommerce.svg';
+import seo from '../assets/seo.svg';
+import maintenance from '../assets/maintenance.svg';
+
+const servicesData = [
+  {
+    id: 1,
+    title: "Web Development",
+    description:
+      "Building responsive and functional websites using the latest technologies.",
+    icon: code,
+  },
+  {
+    id: 2,
+    title: "E-commerce Solutions",
+    description:
+      "Providing comprehensive e-commerce solutions for your business.",
+    icon: ecommerce,
+  },
+  {
+    id: 3,
+    title: "SEO Optimization",
+    description: 
+    "Improving your website's visibility on search engines.",
+    icon: seo,
+  },
+  {
+    id: 4,
+    title: "maintenance & Support",
+    description:
+      "Offering ongoing maintenance and support for your website.",
+    icon: maintenance,
+  }
+];
+
+export default servicesData;
