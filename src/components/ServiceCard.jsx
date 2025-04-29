360e0135cf1ac26df8068b8d2731a95cecafc158
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-04-29 15:53:37 +0100

    minor change

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -3,28 +3,30 @@ import React from "react";
 const ServiceCard = ({ title, description, icon, arrow }) => {
   return (
     <div
-      className="border border-white/40 hover:border-purple-300 hover:bg-purple-400/10 rounded-xl
-                 shadow-md hover:shadow-lg transition-all duration-300
-                 backdrop-blur-md text-center cursor-pointer p-6 group">
-      
+      className="border border-white/40 hover:border-purple-300 hover:bg-purple-400/10 
+                 rounded-xl shadow-md hover:shadow-lg transition-all duration-300
+                 backdrop-blur-sm bg-white/5 text-left cursor-pointer p-6 group"
+    >
       {/* Icon */}
-      <div className="flex items-center justify-center md:justify-start">
-        <div className="rounded-full">
-          <img src={icon} alt={title} className="w-12 h-12 object-contain" />
+      <div className="flex items-center justify-center md:justify-start mb-4">
+        <div className="w-14 h-14 rounded-full bg-white/10 flex items-center justify-center">
+          <img src={icon} alt={title} className="w-8 h-8 object-contain" />
         </div>
       </div>
 
       {/* Title */}
-      <h2 className="text-xl font-semibold text-white group-hover:text-text-header py-2 md:text-left">{title}</h2>
+      <h2 className="text-xl font-semibold text-white group-hover:text-text-header md:text-left">
+        {title}
+      </h2>
 
       {/* Description */}
-      <p className="text-gray-400 group-hover:text-gray-300 text-sm max-w-sm text-center md:text-left block mx-auto">
+      <p className="text-gray-400 group-hover:text-gray-300 text-sm text-left mt-2">
         {description}
       </p>
 
       {/* See More + Arrow */}
-      <div className="flex items-center justify-center md:justify-end mt-4 group">
-        <h3 className="text-center md:text-right text-text-header mt-2 transition-all duration-300 group-hover:text-purple-400">
+      <div className="flex items-center justify-start md:justify-end mt-6 group">
+        <h3 className="text-text-header mt-2 transition-all duration-300 group-hover:text-purple-400">
           See more
         </h3>
         <img
@@ -33,7 +35,6 @@ const ServiceCard = ({ title, description, icon, arrow }) => {
           className="w-3 h-3 object-contain ml-1.5 mt-2 transition-transform duration-300 group-hover:translate-x-1 group-hover:-translate-y-1"
         />
       </div>
-
     </div>
   );
 };
