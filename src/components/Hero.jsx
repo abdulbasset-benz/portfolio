ef3ae033127e4454970c3cde0e10e9a0e3bf6c4b
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-04-29 15:51:52 +0100

    fix margin bottom, created white space

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,32 +5,38 @@ import arrow from "../assets/arrow.svg";
 
 const Hero = () => {
   return (
-    <div className="container mx-auto text-center p-6 mb-16 relative flex flex-col items-center gap-5 text-amber-50 font-montserrat">
-      <img
-        src={avatar}
-        alt="Avatar"
-        className="w-50 h-50 rounded-full shadow-lg"
-      />
-      <p className="uppercase text-text-secondary">
-        Dynamic Web Magic with <span className="text-text-secondary">Laravel</span> &{" "}
-        <span className="text-text-secondary">react js</span>
-      </p>
-      <h1 className="font-inter font-extrabold capitalize text-4xl md:text-5xl lg:text-6xl xl:text-6xl max-w-5xl mx-auto ">
-        Transforming Concepts into Seamless{" "}
-        <span className="text-text-header block md:inline lg:whitespace-nowrap">
-          User Experiences
-        </span>
-      </h1>
-      <h3>Hi! I’m Abdulbasset, a Full-stack Developer based in Algeria.</h3>
-      <button className="btn flex items-center py-3 px-6 border border-gray-500 rounded-md cursor-pointer font-semibold hover:text-slate-300 group">
-        See My Work
+    <section className="bg-primary text-amber-50 font-montserrat py-20">
+      <div className="container mx-auto flex flex-col items-center gap-5 text-center px-4 relative">
         <img
-          src={arrow}
-          alt="Arrow"
-          className="inline-block ml-3 transition-all duration-300 group-hover:translate-x-1 group-hover:-translate-y-1"
+          src={avatar}
+          alt="Avatar"
+          className="w-40 h-40 rounded-full shadow-lg object-cover"
         />
-      </button>
-    </div>
+        <p className="uppercase text-text-secondary tracking-wide">
+          Dynamic Web Magic with <span className="text-text-secondary">Laravel</span> &{" "}
+          <span className="text-text-secondary">React.js</span>
+        </p>
+        <h1 className="font-inter font-extrabold capitalize text-4xl md:text-5xl lg:text-6xl max-w-5xl">
+          Transforming Concepts into Seamless{" "}
+          <span className="text-text-header block md:inline">
+            User Experiences
+          </span>
+        </h1>
+        <h3 className="text-lg md:text-xl">
+          Hi! I’m Abdulbasset, a Full-stack Developer based in Algeria.
+        </h3>
+        <div>
+          <button className="btn px-6 py-3 border border-gray-500 rounded-md font-semibold hover:text-slate-300 group transition duration-300">
+            See My Work
+            <img
+              src={arrow}
+              alt="Arrow"
+              className="inline-block ml-3 transition-transform duration-300 group-hover:translate-x-1 group-hover:-translate-y-1"
+            />
+          </button>
+        </div>
+      </div>
+    </section>
   );
 };
 
