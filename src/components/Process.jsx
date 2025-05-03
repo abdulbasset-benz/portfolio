28029085ae8e83be8299355379c796e85659a2c6
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-05-03 15:30:31 +0100

    finish the process section

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import steps from '../data/Steps';
+import React from "react";
+import steps from "../data/Steps";
 
 const Process = () => {
   return (
-    <div className="max-w-4xl mx-auto px-4 py-12">
-      <h2 className="text-3xl font-bold mb-10 text-center">How I Do It</h2>
-      <div className="relative border-2 border-red-500">
-        {steps.map((step, index) => {
-          const Icon = step.icon;
-          return (
-            <div key={index} className="mb-10 ml-2 relative">
-              <div className="absolute -left-6">
-                <Icon className="w-8 h-8 text-white bg-primary p-2 rounded-full ring-4 ring-white" />
+    <div className=" mx-auto  py-12 bg-primary border-t border-gray-800 p-10">
+      <div className="max-w-4xl mx-auto text-white ">
+        <h1 className="text-5xl font-bold bg-clip-text text-transparent text-center  bg-slate-50">
+          How do I work?
+        </h1>
+        <h3 className="text-xl text-center mb-8 bg-clip-text text-transparent capitalize bg-text-header animate-pulse">
+          ✨ the process ✨
+        </h3>
+        <div className="relative border-l-2  border-l-text-header">
+          {steps.map((step, index) => {
+            const Icon = step.icon;
+            return (
+              <div key={index} className="mb-10 ml-2 relative">
+                <div className="absolute -left-6">
+                  <Icon className="w-8 h-8 text-white bg-primary p-2 rounded-full ring-4 ring-white" />
+                </div>
+                <h3 className="text-xl ml-6 font-semibold text-text-header">
+                  {step.title}
+                </h3>
+                <p className="text-gray-400 mt-2 ml-6">{step.description}</p>
               </div>
-              <h3 className="text-xl ml-6 font-semibold text-gray-800">{step.title}</h3>
-              <p className="text-gray-600 mt-2 ml-6">{step.description}</p>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
