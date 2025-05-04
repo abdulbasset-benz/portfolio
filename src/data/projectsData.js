f3131256a4aef25f047199b16ce0db22b1b78c98
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-05-04 19:47:22 +0100

    prepare the data to be inserted in the projects cards, separate files

---
+++ src/data/projectsData.js
@@ -0,0 +1,31 @@
+const projectsData = [
+  {
+    id: 1,
+    title: "CNEP Bank dashboard",
+    description:
+      "this dashboard is for the CNEP bank in algeria, it is a web application that allows users to manage their bank accounts, view transactions, and perform various banking operations. it also helps decision makers to visualize the data and make informed decisions with clear insights on the activity level of each agency they run.",
+    image: "path/to/image1.jpg",
+    techStack: ["Laravel", "tailwind", "Javascript", "MySQL"],
+    githubUrl: "",
+  },
+  {
+    id: 2,
+    title: "ALMA tourism",
+    description:
+      "website for a travel agency that offers a wide range of travel services, including flight bookings, hotel reservations, and travel packages. The website is designed to be user-friendly and visually appealing, with a focus on providing an excellent user experience.",
+    image: "path/to/image2.jpg",
+    techStack: ["PHP", "bootstrap", "Javascript", "MySQL"],
+    githubUrl: "",
+  },
+  {
+    id: 3,
+    title: "SHOP.CO E-commerce",
+    description:
+      "an e-commerce website that allows users to browse and purchase products online. The website is designed to be user-friendly and visually appealing, with a focus on providing an excellent user experience. It includes features such as product search, shopping cart, and secure payment processing.",
+    image: "path/to/image3.jpg",
+    techStack: ["laravel", "tailwind", "Javascript", "MySQL"],
+    githubUrl: "",
+  },
+];
+
+export default projectsData;
