39d9516ff5e8d36f01142b72d194a8d9aab93127
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-05-04 21:25:28 +0100

    minor chore

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,10 +1,15 @@
+import alma1 from "../assets/images/alma1.png";
+import alma2 from "../assets/images/alma2.png";
+import alma3 from "../assets/images/alma3.png";
+import alma4 from "../assets/images/alma4.png";
+
 const projectsData = [
   {
     id: 1,
     title: "CNEP Bank dashboard",
     description:
       "this dashboard is for the CNEP bank in algeria, it is a web application that allows users to manage their bank accounts, view transactions, and perform various banking operations. it also helps decision makers to visualize the data and make informed decisions with clear insights on the activity level of each agency they run.",
-    image: "path/to/image1.jpg",
+    image: [alma1, alma2, alma3, alma4],
     techStack: ["Laravel", "tailwind", "Javascript", "MySQL"],
     githubUrl: "",
   },
@@ -13,7 +18,7 @@ const projectsData = [
     title: "ALMA tourism",
     description:
       "website for a travel agency that offers a wide range of travel services, including flight bookings, hotel reservations, and travel packages. The website is designed to be user-friendly and visually appealing, with a focus on providing an excellent user experience.",
-    image: "path/to/image2.jpg",
+    image: [alma1, alma2, alma3, alma4],
     techStack: ["PHP", "bootstrap", "Javascript", "MySQL"],
     githubUrl: "",
   },
@@ -22,7 +27,7 @@ const projectsData = [
     title: "SHOP.CO E-commerce",
     description:
       "an e-commerce website that allows users to browse and purchase products online. The website is designed to be user-friendly and visually appealing, with a focus on providing an excellent user experience. It includes features such as product search, shopping cart, and secure payment processing.",
-    image: "path/to/image3.jpg",
+    image: [alma1, alma2, alma3, alma4],
     techStack: ["laravel", "tailwind", "Javascript", "MySQL"],
     githubUrl: "",
   },
