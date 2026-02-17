79ae6e5c8c233cdd00680ebc97628135052482d2
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-02-17 12:47:02 +0100

    Build nav bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,27 @@
 import { TextAlignJustify } from "lucide-react";
+import logo from "../assets/logo.svg";
 
 const Navigation = () => {
   return (
-    <div className="w-full relative">
+    <div className="w-full fixed top-0 left-0">
       <div className="flex justify-between items-center p-2 md:p-4 lg:p-8">
-        <h1 className="font-bold text-2xl lg:text-4xl">BENZY</h1>
+        <h1 className="font-bold font-clash text-2xl uppercase ">
+            logo.
+        </h1>
 
         <div className="flex items-center gap-7 lg:hidden border p-2 md:p-4 rounded-lg">
           <TextAlignJustify />
         </div>
 
-        <div className="hidden lg:flex font-bold leading-tight text-2xl">
-          <ul>
-            <li>Home</li>
-            <li>Work</li>
-            <li>Contact</li>
-            <li>Home</li>
+        <div className="hidden lg:flex gap-4 font-semibold leading-tight text-xl">
+          <ul className=" gap-3">
+            <li className="cursor-pointer">Home</li>
+            <li className="cursor-pointer">Work</li>
+            <li className="cursor-pointer">Contact</li>
+            <li className="cursor-pointer">Home</li>
           </ul>
+
+          {/* <button className="bg-[#FF3419] px-4 py-3 text-white rounded-lg">Book a Call</button> */}
         </div>
       </div>
     </div>
