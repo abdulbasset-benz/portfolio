587f97437f541bf6d9dbef5ae25deaf27e1ff24f
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-02-22 16:06:09 +0100

    Implement the services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <div className="h-full w-screen overflow-hidden px-2 py-10">
+      <div className=" flex flex-col">
+        <div className="py-20">
+          <h1 className="font-inter font-bold text-5xl text-left">
+            Services
+          </h1>
+        </div>
 
-export default Services
+        <div className="flex flex-col items-center gap-10">
+            <div className="service-section relative w-full flex items-center justify-center border-b border-gray-300 p-4">
+                <h1 className="font-instru text-4xl font-bold">UI/UX Design</h1>
+                <span className="absolute top-0 right-3 text-gray-400">01</span>
+            </div>
+            <div className="service-section relative w-full flex items-center justify-center border-b border-gray-300 p-4">
+                <h1 className="font-instru text-4xl font-bold">Web Development</h1>
+                <span className="absolute top-0 right-3 text-gray-400">02</span>
+            </div>
+            <div className="service-section relative w-full flex items-center justify-center border-b border-gray-300 p-4">
+                <h1 className="font-instru text-4xl font-bold">Website Redesign</h1>
+                <span className="absolute top-0 right-3 text-gray-400">03</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
