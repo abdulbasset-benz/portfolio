df2901dfc7ef296e0398ee90a35ce844f81dac9a
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-02-22 14:42:24 +0100

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { ChevronLast } from "lucide-react"
 import Navigation from "./components/Navigation"
+import Hero from "./components/Hero"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
    <section className="">
     <Navigation />
+    <Hero />
    </section>
   )
 }
