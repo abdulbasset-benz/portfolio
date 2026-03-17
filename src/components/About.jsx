8ffb211ffe4c61e78e906181dc1f20a53f56e42e
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-03-17 17:50:35 +0100

    implement new copy to the about section that is more aligned with what i stand for

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+const About = () => {
+  return (
+    <div className="h-full w-screen overflow-hidden relative p-2">
+      <div className="py-20">
+        <h1 className="font-inter font-bold text-5xl text-left">About</h1>
+      </div>
+      <div>
+        <h1 className="font-inter font-medium text-3xl text-gray-700">
+          I build digital experiences that are simple, fast, and
+          conversion-driven. My focus isn’t just on building websites — it’s on
+          helping businesses achieve real results. Whether it’s increasing
+          sales, improving visibility, or generating more leads, every solution
+          I create is designed with that goal in mind.
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default About;
