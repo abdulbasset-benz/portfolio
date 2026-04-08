e1519f021ff5d1c322702889f5412fd24b20dd41
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-04-08 16:02:57 +0100

    Implement responsive design for projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,51 +2,70 @@ import archi from "../assets/archis.jpg";
 
 const Projects = () => {
   return (
-    <div className="h-full overflow-hidden">
-      <div className="py-20 px-2">
-        <h1 className="font-inter font-bold text-5xl text-left">
-          Latest Projects
+    <div className="h-full overflow-hidden md:p-10">
+      <div className="py-5 flex justify-center">
+        <h1 className="font-clash font-medium text-[clamp(3.8rem,10vw,7rem)] leading-[clamp(1em,0.7em,2em)] text-center">
+          <span className="text-primary text-8xl leading-none relative z-10">
+            •
+          </span>{" "}
+          Latest <br /> Projects
         </h1>
       </div>
       {/* projects section */}
-      <div className="flex flex-col gap-10">
-        <div className="project-card p-2 flex flex-col gap-4">
+      <div className="flex flex-col items-center gap-30">
+        <div className="project-card p-2 flex flex-col gap-4 max-w-3xl">
           <div className="project-img rounded-md overflow-hidden">
             <img src={archi} alt="" />
           </div>
           <div className="project-info flex items-center justify-between">
-            <h1 className="font-clash text-lg">Project Title</h1>
-            <h3 className="font-clash text-lg">UI/UX</h3>
+            <h1 className="font-clash font-semibold text-[clamp(1rem,4vw,2rem)] text-gray-900">
+              Architex
+            </h1>
+            <h3 className="font-clash text-[clamp(1rem,4vw,1.5rem)] text-gray-400 ">
+              UI/UX - Web Dev
+            </h3>
           </div>
         </div>
 
-        <div className="project-card p-2 flex flex-col gap-4">
+        <div className="project-card p-2 flex flex-col gap-4 max-w-3xl">
           <div className="project-img rounded-md overflow-hidden">
             <img src={archi} alt="" />
           </div>
           <div className="project-info flex items-center justify-between">
-            <h1 className="font-clash text-lg">Project Title</h1>
-            <h3 className="font-clash text-lg">UI/UX</h3>
+            <h1 className="font-inter text-[clamp(1rem,4vw,1.5rem)] text-gray-900">
+              Miss Kitty
+            </h1>
+            <h3 className="font-clash text-[clamp(1rem,4vw,1.5rem)] text-gray-400">
+              UI/UX
+            </h3>
           </div>
         </div>
 
-        <div className="project-card p-2 flex flex-col gap-4">
+        <div className="project-card p-2 flex flex-col gap-4 max-w-3xl">
           <div className="project-img rounded-md overflow-hidden">
             <img src={archi} alt="" />
           </div>
           <div className="project-info flex items-center justify-between">
-            <h1 className="font-clash text-lg">Project Title</h1>
-            <h3 className="font-clash text-lg">UI/UX</h3>
+            <h1 className="font-clash font-semibold text-[clamp(1rem,4vw,1.5rem)] text-gray-900">
+              Benak Hills
+            </h1>
+            <h3 className="font-clash text-[clamp(1rem,4vw,1.5rem)] text-gray-400">
+              UI/UX
+            </h3>
           </div>
         </div>
 
-        <div className="project-card p-2 flex flex-col gap-4">
+        <div className="project-card p-2 flex flex-col gap-4 max-w-3xl">
           <div className="project-img rounded-md overflow-hidden">
             <img src={archi} alt="" />
           </div>
           <div className="project-info flex items-center justify-between">
-            <h1 className="font-clash text-lg">Project Title</h1>
-            <h3 className="font-clash text-lg">UI/UX</h3>
+            <h1 className="font-clash font-semibold text-[clamp(1rem,4vw,1.5rem)] text-gray-900">
+              Archite Y
+            </h1>
+            <h3 className="font-clash text-[clamp(1rem,4vw,1.5rem)] text-gray-400">
+              Web Development
+            </h3>
           </div>
         </div>
       </div>
