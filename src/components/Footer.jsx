d2d14924c8c99ebf70fd4ac3e0648ceda95e4c25
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-04-08 15:09:34 +0100

    Polish footer section for mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="w-screen p-10">
+      <div className="flex flex-col items-center ">
+        <h1 className="font-clash font-bold text-6xl capitalize">abu nazim</h1>
+        <p className="font-inter text-center text-gray-500">
+          Elelvating Brands through Design and Communication
+        </p>
+      </div>
+      {/* social links */}
+      <div className="py-10 flex justify-between items-center">
+        <div className="font-clash">Instagram</div>
+        <div className="font-clash">LinkedIn</div>
+        <div className="font-clash">WhatsApp</div>
+      </div>
 
-export default Footer
+      <div className="pt-10">
+        <ul className="flex flex-col items-center gap-2">
+          <li>
+            <a className="capitalize font-clash font-normal text-lg" href="">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="capitalize font-clash font-normal text-lg" href="">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="capitalize font-clash font-normal text-lg" href="">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a className="capitalize font-clash font-normal text-lg" href="">
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="capitalize font-clash font-normal text-lg" href="">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
