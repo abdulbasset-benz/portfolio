a307bdb69bb2137bf3ab6614201be6ca27922e26
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-04-08 14:35:10 +0100

    Add the footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import Navigation from "./components/Navigation"
-import Hero from "./components/Hero"
-import Porjects from "./components/Projects"
-import Services from "./components/Services"
-import Cta from "./components/Cta"
-import About from "./components/About"
-
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+import Porjects from "./components/Projects";
+import Services from "./components/Services";
+import Cta from "./components/Cta";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
-  
-
   return (
-   <section className="">
-    <Navigation />
-    <Hero />
-    <Porjects />
-    <About />
-    <Services />
-    <Cta />
-   </section>
-  )
+    <section className="">
+      <Navigation />
+      <Hero />
+      <Porjects />
+      <About />
+      <Services />
+      <Cta />
+      <Footer />
+    </section>
+  );
 }
 
-export default App
+export default App;
