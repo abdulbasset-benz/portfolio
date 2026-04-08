9528095f399156c085273eb5a45b0d809c59059e
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-04-08 15:37:59 +0100

    Work on the responsive design for all screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,21 @@
 const Hero = () => {
   return (
-    <div className="pt-15 h-screen">
+    <div className="pt-24 h-screen">
       <div className="flex flex-col items-center justify-around h-full p-2 gap-7">
         <div className="space-y-5">
-          <h1 className="text-center font-clash font-bold uppercase text-6xl">
-            digital designer
+          <h1 className="text-center font-clash font-bold uppercase text-[clamp(3.8rem,10vw,7rem)] leading-[clamp(1em,-0.02em,0.02em)]">
+            digital <br />designer
           </h1>
-          <p className="text-center font-instru text-2xl font-medium text-gray-600">
+          <p className="text-center max-w-xl lg:max-w-2xl font-inter text-[clamp(1rem,2.5vw,1.5rem)] text-gray-600">
             hey there, i'm Abdulbasset, a digital designer with a passion for
             transforming ideas into captivating visual experiences
           </p>
         </div>
 
-        <div className="w-full">
+        <div className="w-full md:w-1/2 mx-auto">
           <ul className="flex justify-between">
-            <li className="font-clash font-thin">UI/UX design</li>
-            <li className="font-clash font-thin">Web development</li>
+            <li className="font-clash font-normal text-gray-800 text-[clamp(1rem,1.8vw,1.5rem)] border-b border-dashed px-2 border-primary">UI/UX design</li>
+            <li className="font-clash font-normal text-gray-800 text-[clamp(1rem,1.8vw,1.5rem)] border-b border-dashed px-2 border-primary">Web development</li>
           </ul>
         </div>
 
@@ -36,7 +36,7 @@ const Hero = () => {
             </span>
           </span>
 
-          <h1 className="font-medium font-inter">
+          <h1 className="font-medium font-clash">
             {" "}
             Available for freelance projects
           </h1>
