66813105864fadec340f44eb9765e8fb715b1314
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-04-09 18:36:04 +0100

    Install react router

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,12 @@
+import { NavLink } from "react-router";
 import { TextAlignJustify } from "lucide-react";
 
 const Navigation = () => {
   return (
     <div className="w-full fixed top-0 left-0">
       <div className="flex justify-between items-center p-2 md:p-4 lg:p-8 max-w-325 mx-auto">
-        <h1 className="font-bold font-clash text-[clamp(1.2rem,2vw,1.5rem)] uppercase text-primary ">
-            Abdulbasset-benz
+        <h1 className="font-bold font-clash text-[clamp(1.2rem,2vw,1.5rem)] uppercase text-primary">
+          Abdulbasset-benz
         </h1>
 
         <div className="flex items-center gap-7 lg:hidden border p-2 md:p-4 rounded-lg">
@@ -14,13 +15,39 @@ const Navigation = () => {
 
         <div className="hidden lg:flex gap-5 font-semibold font-inter leading-tight text-xl">
           <ul className="flex items-center gap-5 text-gray-700">
-            <li className="cursor-pointer">Home</li>
-            <li className="cursor-pointer">Projects</li>
-            <li className="cursor-pointer">About</li>
-            <li className="cursor-pointer">Contact</li>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "text-primary" : "")}
+              >
+                Home
+              </NavLink>{" "}
+            </li>
+            <li>
+              <NavLink
+                to="/projects"
+                className={({ isActive }) => (isActive ? "text-primary" : "")}
+              >
+                Projects
+              </NavLink>{" "}
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={({ isActive }) => (isActive ? "text-primary" : "")}
+              >
+                About
+              </NavLink>{" "}
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                className={({ isActive }) => (isActive ? "text-primary" : "")}
+              >
+                Contact
+              </NavLink>{" "}
+            </li>
           </ul>
-
-          {/* <button className="bg-[#FF3419] px-4 py-3 text-white rounded-lg">Book a Call</button> */}
         </div>
       </div>
     </div>
