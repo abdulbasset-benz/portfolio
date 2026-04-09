eebaae13f77ecb56f47219351c4d00a896ccf89a
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-04-09 18:34:57 +0100

    Add project pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero'
+import Projects from '../components/Projects'
+import About from '../components/About'
+import Services from '../components/Services'
+import Cta from '../components/Cta'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <About />
+      <Services />
+      <Cta />
+    </>
+  )
+}
