571fb752f95a120b186a0f26818a13bbd52d1693
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-04-09 19:25:14 +0100

    Install react hook form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="h-full flex items-center justify-center">
+        <div className="form h-screen w-300 bg-gray-500 py-20">
+            <h1 className="font-inter capitalize font-semibold text-center text-2xl">contact us</h1>
+        </div>
+    </div>
   )
 }
 
