4a3065f8368ac502b914b59b97df827d45a24793
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-04-09 18:35:51 +0100

    Add project routing with proper redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import Navigation from "./components/Navigation";
-import Hero from "./components/Hero";
-import Porjects from "./components/Projects";
-import Services from "./components/Services";
-import Cta from "./components/Cta";
-import About from "./components/About";
-import Footer from "./components/Footer";
+import { Routes, Route } from 'react-router'
+import RootLayout from './Layouts/RootLayout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
 
 function App() {
   return (
-    <section className="overflow-hidden">
-      <Navigation />
-      <Hero />
-      <Porjects />
-      <About />
-      <Services />
-      <Cta />
-      <Footer />
-    </section>
-  );
+    <Routes>
+      <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  )
 }
 
-export default App;
+export default App
