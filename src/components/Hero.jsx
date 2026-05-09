0d13c4b5df0e975a662055c64279464ef3e2f5ce
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-05-09 23:14:58 +0100

    Polish section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router";
 const Hero = () => {
   return (
     <div className="pt-24 min-h-screen flex items-center">
-      <div className="flex flex-col items-center justify-center w-full px-4 sm:px-6 lg:px-8 gap-8 py-12">
+      <div className="flex flex-col items-center justify-center w-full px-4 sm:px-6 lg:px-8 gap-15 py-12">
 
         {/* Headline + subheadline */}
         <div className="space-y-5 text-center max-w-5xl mx-auto">
-          <h1 className="font-clash font-bold uppercase text-center tracking-normal leading-[0.95] text-[clamp(2.8rem,8vw,5rem)]">
-            Websites for Luxury Real Estate & Interior Design Brands
+          <h1 className="font-instru font-bold capitalize text-center tracking-normal leading-[0.95] text-[clamp(4rem,8vw,10rem)]">
+            Digital Presence That Stands Out
           </h1>
           <p className="text-center mx-auto max-w-xl font-inter text-[clamp(1rem,2vw,1.25rem)] text-gray-500 leading-relaxed">
             I'm Abdulbasset — a fullstack developer and designer who builds
@@ -18,7 +18,7 @@ const Hero = () => {
         </div>
 
         {/* Services list */}
-        <ul className="flex flex-col sm:flex-row items-center justify-center gap-6 sm:gap-16 w-full max-w-lg">
+        <ul className="flex sm:flex-row items-center justify-center gap-6 sm:gap-16 w-full max-w-lg">
           <li className="font-clash font-normal text-gray-800 text-[clamp(0.95rem,1.6vw,1.25rem)] border-b border-dashed border-primary px-2 pb-0.5 text-center">
             UI/UX Design
           </li>
