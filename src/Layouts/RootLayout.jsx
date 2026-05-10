94b1d46bd5433db03a8ab81e1b1b2fbb8b8ddc5e
abdulbasset-benz <benz.abdelbasset@gmail.com>
2026-05-10 12:29:48 +0100

    Add scroll to top comp

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,15 +1,17 @@
-import { Outlet } from 'react-router'
-import Navigation from '../components/Navigation'
-import Footer from '../components/Footer'
+import { Outlet } from "react-router";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function RootLayout() {
   return (
     <section className="overflow-hidden">
+      <ScrollToTop /> {/* ← add this */}
       <Navigation />
       <main>
         <Outlet />
       </main>
       <Footer />
     </section>
-  )
+  );
 }
